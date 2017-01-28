b91e6237718ef2bbfe998b3a34c68f44d264b87d
djukadjuka <milanns@live.com>
2017-01-28 19:31:14 +0100

    Labeled and extracted regions from thresholded image. All regions parsed and height/width ratios extracted. Found histogram from regions and their bounds, this provided a max and min bound for good regions. Started mapping good regions with the provided min max ratios

--- Image_processing.py
+++ Image_processing.py
@@ -30,23 +30,82 @@ def load_and_get_images(full_path_to_image):                #... (1) tested OK!
                                    (con.RESIZE_WIDTH,
                                     con.RESIZE_HEIGHT))
 
+    #get ordinary thresholded image
     img,t_image = cv2.threshold(gray_image_resized.copy(),
                                 127,
                                 255,
                                 cv2.THRESH_BINARY_INV)
+    #create kernel for extended thresholding
+    #options
     kernel = np.ones((1,1),
                      np.uint8)
+
+    #open up image to remove small dots and
+    #interfierence
     t_image_fix = cv2.morphologyEx(t_image,
                                    cv2.MORPH_OPEN,
                                    kernel)
 
     return original_image,original_image_resized, gray_image, gray_image_resized, t_image, t_image_fix
 
-def get_resized_images(full_path_to_image):
+#shorthand for getting necessary imgs
+def get_resized_images(full_path_to_image):                 #...(1)
     original_image, original_image_r, gray_image, gray_r, thresh, thresh_fix = load_and_get_images(full_path_to_image)
     return original_image_r, gray_r, thresh_fix
 
+#get labeled regions
+def get_labeled_regions(thresh_image):                      #...(2)
+    labeled_image = label(thresh_image) #just label out the thresholded image
+                                        #for extracting regions later
+    regions = regionprops(labeled_image)
+    return labeled_image,regions
 
+#get image ratios based on height and width
+#all training images are black and white
+#only the height and width ratios are of interest
+#on a resized image of a licence plate
+#these ratios will @probably be the same...
+def get_region_ratios(all_regions):                      #...(3)
 
+    #list for storing all ratios
+    all_ratios = []
+    for region in all_regions:
+
+        #get bounding box from current region
+        bbox = region.bbox
+
+        #create width and height for region
+        h = bbox[2] - bbox[0]
+        w = bbox[3] - bbox[1]
+
+        #create the ratio
+        ratio = float(h)/w
+        all_ratios.append(ratio)
+    return all_ratios
+
+#create top and bottom bounds for a region group
+def get_region_bounds(all_ratios):                      #...(4)
+
+    #parameters gained after testing
+    #and trial and error in previous scraped project
+    n,bins,patches = plt.hist(all_ratios,bins=range(0,10,3))
+    #plt.show()     #-->for showing the histogram for proof
+    
+    #get the index where the histogram
+    #has a maximum y value
+    max_bins = np.wher(n==n.max())
+    min_max_pairs = []
+    for b in max_bins[0]:               #because the 0th
+                                        #element is an array
+        min_max_pair = [bins[b],bins[b+1]]
+        min_max_pairs.append(min_max_pair)
+    return min_max_pairs
+
+#get the regions that are within the bounds
+#because the threshed image may have
+#picked up interfierence that was not opened
+    #may be overkill....
+def get_target_regions(all_regions,min_max_pairs):
+    pass
 #Image_processing.py
 #Import -- ip
