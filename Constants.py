b382610a3f8b785f931b43d002d434d0349619ec
djukadjuka <milanns@live.com>
2017-01-28 18:21:14 +0100

    Added training data image names and extension to Constants.py

--- Constants.py
+++ Constants.py
@@ -1,4 +1,29 @@
 #Constants.py
 
+#########################
+#MISCELLANIOUS CONSTANTS#
+#########################
+TRAINING_DATA_IMAGE_SUFFIX = ".JPG"
+
+#############
+#IMAGE SIZES#
+#############
+RESIZE_WIDTH = 300      #For resizing processed images, contours etc...
+RESIZE_HEIGHT = 150     #
+
+LETTER_WIDTH = 30       #For resizing individual letters when creating training data
+LETTER_HEIGHT = 20      #
+
+#######
+#FILES#
+#######
+TRAINIG_DATA_IMAGE_NAMES = ["A","B","C","D","E",
+                             "F","G","H","I","J",
+                             "K","L","M","N","O",
+                             "P","Q","R","S","T",
+                             "U","V","W","X","Y",
+                             "Z","0","1","2","3",
+                             "4","5","6","7","8","9"]
+
 #Constants.py
 #Import -- as con
