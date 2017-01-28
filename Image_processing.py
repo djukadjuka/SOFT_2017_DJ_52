8d8f2afc8efb5ee3705115bc779643a0f0f1261a
djukadjuka <milanns@live.com>
2017-01-28 18:29:44 +0100

    Started work on image processing and installer skimage and matplotlib

--- Image_processing.py
+++ Image_processing.py
@@ -1,4 +1,14 @@
 #Image_processing.py
 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from skimage.measure import regionprops
+from skimage.measure import label
+from skimage.exposure import histogram
+
+import Constants as con
+
 #Image_processing.py
 #Import -- ip
